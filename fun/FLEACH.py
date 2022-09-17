57eea6d0aa1a6000b11668c3f12628727a1b87fe
Czarko <czarek93@op.pl>
2022-09-17 16:11:13 +0200

    działa, sprawdzic ostatnie to do i mozna wrzzucac do worda

--- fun/FLEACH.py
+++ fun/FLEACH.py
@@ -185,7 +185,8 @@ class FLEACHSimulation:
             self.__cluster_head_selection_phase(round_number)
             self.no_of_ch = len(self.list_CH)  # Liczba głów klastrów
 
-            plotter2.start(self.Sensors, self.model, round_number)
+            if round_number == 150 or round_number == self.model.rmax:
+                plotter2.start(self.Sensors, self.model, round_number)
 
             # #################################################
             # ############# faza stanu ustalonego #############
